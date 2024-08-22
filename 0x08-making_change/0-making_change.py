2f08db10c79d25c90175a39bfb16be67aa4084b2
Anoonaa <anoonasithole0@gmail.com>
2024-08-22 16:56:44 -0700

    refactor + added test

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-""" Question : fewest number of coins needed to
-meet a given amount total
+"""
+Function to determine the fewest number of coins needed to meet a given amount total.
 """
 
 def makeChange(coins, total):
-    """ fewest number of coins needed to meet total """
     if total <= 0:
         return 0
-    # sort the coins in descending order
+
     coins.sort(reverse=True)
-    change = 0
+    count = 0
     for coin in coins:
-        if total <= 0:
-            break
-        temp = total // coin
-        change += temp
-        total -= (temp * coin)
-    if total != 0:
-        return -1
-    return change
+        while total >= coin:
+            total -= coin
+            count += 1
+        if total == 0:
+            return count
+    
+    return -1
+
